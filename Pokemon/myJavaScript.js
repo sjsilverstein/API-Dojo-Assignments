8840457cd620c7a6476f5a12a32cb49b5a6e9999
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-03-01 17:08:18 -0800

    Pulled API Sprites

--- Pokemon/myJavaScript.js
+++ Pokemon/myJavaScript.js
@@ -1,7 +1,18 @@
 // Stephen Silverstein
 // https://github.com/sjsilverstein
-
 $(document).ready(function(){
 // BODY
+	for(var i = 1; i <=151; i++){
+	$('.images').append(`
+		<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png" alt="Pokemon No: ${i}" id="${i}" class="sprite">
+		`);
+	}
+
+	// $('.images img').click(function(){
+	// 	console.log($(this).attr("id"));
+	// 	$('.dexinfo').html(`
+	// 		<h1>Name</h1>
+	// 		`);
+	// })
 
 })
